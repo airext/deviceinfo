1442fff40240175f7b9d02eb8027b1f87bf9bb3a
Max Rozdobudko <max.rozdobudko@gmail.com>
2018-04-01 22:12:29 +0300

    Adds GetSafe method on Android

--- deviceinfo-android/DeviceInfo/src/main/java/com/github/airext/deviceinfo/functions/GetSafeAreaFunction.java
+++ deviceinfo-android/DeviceInfo/src/main/java/com/github/airext/deviceinfo/functions/GetSafeAreaFunction.java
@@ -1,5 +1,7 @@
 package com.github.airext.deviceinfo.functions;
 
+import android.app.Activity;
+import android.support.v4.view.ViewCompat;
 import com.adobe.fre.FREContext;
 import com.adobe.fre.FREFunction;
 import com.adobe.fre.FREObject;
@@ -12,6 +14,15 @@ public class GetSafeAreaFunction implements FREFunction {
 
     @Override
     public FREObject call(FREContext context, FREObject[] args) {
+
+        Activity activity = context.getActivity();
+
+        ViewCompat.onApplyWindowInsets()
+
+        FREObject top = FREObject.newObject(activity.view)
+
+//        FREObject insets = FREObject.newObject()
+
         // TODO: Not yet implemented
         return null;
     }
